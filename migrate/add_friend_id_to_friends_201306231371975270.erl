f2329f679e395d5417f7d1da534a96121af4e410
Allen Zhuo <zhuoyikang@gmail.com>
2013-06-28 21:29:11 +0800

    off database name

--- migrate/add_friend_id_to_friends_201306231371975270.erl
+++ migrate/add_friend_id_to_friends_201306231371975270.erl
@@ -2,7 +2,7 @@
 -export([up/0, down/0]).
 
 up() ->
-  Query = "ALTER TABLE `slg_server`.`friends` ADD COLUMN `friend_id` BIGINT(20) NOT NULL  AFTER `user_id` ;
+  Query = "ALTER TABLE `friends` ADD COLUMN `friend_id` BIGINT(20) NOT NULL  AFTER `user_id` ;
   ",
   model_exec:exec(migrate, Query),
   ok.
